dddc896c5f139605cc4a300776743f1623c912e1
thuquang2107 <anhthuhoang2107@gmail.com>
2024-08-25 11:24:46 +0700

    script2.js

--- script2.js
+++ script2.js
@@ -11,8 +11,8 @@ document.addEventListener('DOMContentLoaded', function() {
             if (progress >= 100) {
                 clearInterval(interval);
                 setTimeout(() => {
-                    loaderOverlay.style.transform = 'translate3d(0px, -100%, 0px)'; 
-                    loaderOverlay.style.opacity = '1';
+                    loaderOverlay.style.transform = 'translate3d(0px, -100%, 0px)'; // Move out of the viewport upwards
+                    loaderOverlay.style.opacity = '0';
                     setTimeout(() => {
                         loaderOverlay.style.display = 'none';
                     }, 1500); // Match the CSS transition duration
@@ -20,31 +20,10 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         }, 100); 
 
-        // Mark the preloader as shown
+        
         sessionStorage.setItem('preloaderShown', 'true');
     } else {
-        // Immediately hide the loader if it has been shown before
+       
         loaderOverlay.style.display = 'none';
     }
 });
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     const loaderOverlay = document.querySelector('.loader-overlay');
-
-//     // Simulate loading progress
-//     let progress = 0;
-//     const interval = setInterval(() => {
-//         progress += 10;
-        
-//         if (progress >= 100) {
-//             clearInterval(interval);
-//             setTimeout(() => {
-//                 loaderOverlay.style.transform = 'translate3d(0px, -100%, 0px)'; // Move out of the viewport upwards
-//                 loaderOverlay.style.opacity = '0';
-//                 setTimeout(() => {
-//                     loaderOverlay.style.display = 'none';
-//                 }, 1500); // Match the CSS transition duration
-//             }, 500); // Delay for a smoother transition
-//         }
-//     }, 100); // Update progress every 100ms
-// });
