9dd18f3d6ed69faf00c6e6b6077bffe67a76d7dd
thuquang2107 <anhthuhoang2107@gmail.com>
2024-08-25 11:23:42 +0700

    script1.js

--- script1.js
+++ script1.js
@@ -3,10 +3,48 @@ document.addEventListener('DOMContentLoaded', function() {
     const homeContainer = document.querySelector('.home_container');
     const nav = document.querySelector('nav');
     
-    // Add animation classes to trigger CSS animations
     homeContainer.classList.add('animateHomeContainer');
     if (nav) {
-        // Add the animation class to the nav element
         nav.classList.add('animateNav');
     }
 });
+
+
+
+/////different script to ensure that the sticker-set and draggable not overlayed
+///////draggable_home
+document.querySelectorAll('.des_text').forEach(item => {
+    item.addEventListener('dragstart', dragStart);
+    item.addEventListener('dragend', dragEnd);
+});
+
+let activeElement = null;
+let offsetX = 0;
+let offsetY = 0;
+
+function dragStart(e) {
+    activeElement = e.target;
+    offsetX = e.clientX - activeElement.getBoundingClientRect().left;
+    offsetY = e.clientY - activeElement.getBoundingClientRect().top;
+}
+
+function dragEnd(e) {
+    activeElement.style.transform = `translate(${e.clientX - offsetX}px, ${e.clientY - offsetY}px)`;
+    activeElement = null;
+}
+function updateDragEvents() {
+    if (window.innerWidth <= 1024) { ///// Adjust this width as needed
+        removeDragEvents();
+    } else {
+        handleDragEvents();
+    }
+}
+
+
+updateDragEvents();
+
+window.addEventListener('resize', updateDragEvents);
+
+
+
+
